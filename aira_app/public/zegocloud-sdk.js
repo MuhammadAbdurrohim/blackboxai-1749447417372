010d442da6500b7d493df9364a515f31cd3224e0
Yeah! <85945415+MuhammadAbdurrohim@users.noreply.github.com>
2025-06-09 12:39:38 +0700

    Add aira_app/public/zegocloud-sdk.js 1749447578281

---
+++ aira_app/public/zegocloud-sdk.js
@@ -0,0 +1,4 @@
+// Placeholder for ZEGOCLOUD SDK JavaScript file.
+// You should download the official ZEGOCLOUD SDK from their website or CDN and place it here.
+// For example, you can add the following script tag in your blade template to load from CDN:
+// <script src="https://cdn.zegocloud.com/zego-express-1.0.0.js"></script>
